e3e0b6a5b6a2101d00b798e8e3b25044bd973ae4
Riley Marcum <rmarcum13@gmail.com>
2025-01-28 08:19:02 -0500

    add hover states to buttons

--- src/components/BudgetButton.jsx
+++ src/components/BudgetButton.jsx
@@ -7,12 +7,14 @@ export default function BudgetButton({ title, remaining, max }) {
 
   return (
     <div>
-      <h2 className="text-slate-300 text-center text-[30px] mb-2">{title}</h2>
-      <div className="border-8 rounded-3xl p-2">
-        <p className="text-[75px] text-slate-300 font-extrabold text-left pl-4">
+      <h2 className="text-slate-300 font-semibold text-center text-[30px] mb-2">
+        {title}
+      </h2>
+      <div className="border-8 border-slate-300 rounded-3xl p-2 hover:bg-slate-300 group">
+        <p className="text-[75px] text-slate-300 font-extrabold text-left pl-4 group-hover:text-slate-800">
           {`$${remaining}`}
         </p>
-        <p className="text-[36px] text-slate-300 font-normal text-right pr-4 pb-2">
+        <p className="text-[36px] text-slate-300 font-normal text-right pr-4 pb-2 group-hover:text-slate-800">
           {` / $${max}`}
         </p>
         <div
